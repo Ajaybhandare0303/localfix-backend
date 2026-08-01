1c747f3d6006f20cac08f4ee7c9b156d3ac77c8e
ajaybhandare88 <ajaybhandare0803@gmail.com>
2026-08-01 19:31:42 +0530

    Initial Spring Boot project setup

---
+++ src/test/java/com/localfix/LocalFixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.localfix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocalFixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
