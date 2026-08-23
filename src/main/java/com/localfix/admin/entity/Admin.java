f5b3fd44567de2a807894611b9cdb887ef903731
ajaybhandare88 <ajaybhandare0803@gmail.com>
2026-08-23 20:08:18 +0530

    Admin Entity class  add

--- src/main/java/com/localfix/admin/entity/Admin.java
+++ src/main/java/com/localfix/admin/entity/Admin.java
@@ -1,4 +1,14 @@
 package com.localfix.admin.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+
+import java.util.UUID;
+
+@Entity
 public class Admin {
+
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
 }
